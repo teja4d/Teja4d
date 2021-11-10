fd02dc9521705bb26abcfd94427370d27af919fd
DharmaTeja <teja4d@DharmaTejas-MacBook-Air.local>
2021-11-10 21:11:10 +0000

    added vibration

--- src/components/socialicons/SocialIcons.js
+++ src/components/socialicons/SocialIcons.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faFacebook,faLinkedin,faGithub,faTwitter, faFacebookF, faLinkedinIn, faGithubAlt} from '@fortawesome/free-brands-svg-icons'
+import {faFacebook,faLinkedin,faGithub,faTwitter} from '@fortawesome/free-brands-svg-icons'
 
 import './SocialIcons.css'
 export default function SocialIcons() {
+    const icons = [faFacebook,faLinkedin,faGithub,faTwitter]
+    const IconClicked = ()=>{
+        navigator.vibrate(300);
+        
+    }
     return (
         <div>
             <div className='icon-wrapper'>
-           <FontAwesomeIcon className="list-items" icon={faFacebook} size='2x'></FontAwesomeIcon> 
-           <FontAwesomeIcon className="list-items" icon={faLinkedinIn} size='2x'></FontAwesomeIcon> 
-           <FontAwesomeIcon className="list-items" icon={faGithub} size='2x'></FontAwesomeIcon> 
-           <FontAwesomeIcon className="list-items" icon={faTwitter} size='2x'></FontAwesomeIcon> 
+                {icons.map((icon,index)=><FontAwesomeIcon key={index} className="list-items" onClick={IconClicked} icon={icon} size='2x'></FontAwesomeIcon> )}
            </div>
         </div>
     )
