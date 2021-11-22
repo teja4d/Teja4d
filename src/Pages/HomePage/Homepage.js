137cf54385a5ad94694c9ad612a33c6a308f398c
DharmaTeja <teja4d@DharmaTejas-MacBook-Air.local>
2021-11-22 03:10:46 +0000

    added background

--- src/Pages/HomePage/Homepage.js
+++ src/Pages/HomePage/Homepage.js
@@ -4,15 +4,20 @@ import './HomePage.css';
 import Content from '../../components/Content/Content'
 import Image from '../../components/Imagebox/Image';
 import SocialIcons from '../../components/socialicons/SocialIcons';
-
+import { VideoBg, VideoContainer } from '../../components/VideoBackground/VideoBg';
+import video from '../../assets/videos/video.mp4'
+import { useRef } from 'react';
 
 
 export default function HomePage({itemClicked}) {
-  
+    const videoRef = useRef(null);
   return (
     <div className="home-page" onClick={itemClicked}>
-      
       <div className='home-wrapper'>
+      <VideoContainer style={{opacity:0.5}}>
+        <VideoBg ref={videoRef} autoPlay loop muted playback src={video}/>
+      </VideoContainer>
+      <div style={{zIndex:5,position:'relative'}}>
       <Image/>
       
       <h4 className="content-title">--Dharma Teja Vytla--</h4>
@@ -20,6 +25,7 @@ export default function HomePage({itemClicked}) {
       <h4 className='nick-name'>Teja4D</h4>
       <SocialIcons/>
       <Content></Content>
+      </div>
       </div>
       </div>
   );
---
+++ src/components/VideoBackground/VideoBg.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export const VideoBg = styled.video`
+width:100%;
+height:100%;
+object-fit:cover;
+background:#22a34;
+overflow:hidden;`
+
+export const VideoContainer = styled.div`
+position:absolute;
+top:0;
+bottom:0;
+left:0;
+right:0;
+width:100%;
+height:100%;
+z-index:1;
+`
