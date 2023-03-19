a894e54b98c35c78f9a5636a08710d1eb58b6da9
DharmaTeja Vytla <teja6d@gmail.com>
2023-03-19 21:28:52 +0000

    removed colors for text

--- src/Pages/HomePage/Homepage.js
+++ src/Pages/HomePage/Homepage.js
@@ -7,7 +7,7 @@ import {Container } from 'react-bootstrap';
 
 
 export default function HomePage({itemClicked}) {
-  let name ="I'm DharmaTeja Vytla";
+  let name ="DharmaTeja Vytla";
   useEffect(() => {
     window.scrollTo(0, 0)
   }, [])
@@ -16,15 +16,21 @@ export default function HomePage({itemClicked}) {
       <div className='homeBackground' >
       <Image/>
       <Container className="mt-5">
-      <div>
-      <span>
-      {[...name].map(x=>(
-      <span className="display-2 display-md-4 content-title" style={{color:`hsl(${Math.floor(Math.random()*360)},100%,50%)`}}>{x}</span>
-      ))}
+      <div className="d-none d-lg-block my-2">
+      <span className="display-2 content-title">
+      {name}
       </span>
-      <h3 className="content-subtitle">
+      <h3 className="content-subtitle ">
         Full Stack Developer
       </h3>
+      </div>
+      <div className="d-md-none">
+      <span className="display-3 content-title">
+      {name}
+      </span>
+      <p className="content-subtitle my-2 h3-lg ">
+        Full Stack Developer
+      </p>
       </div>
       <SocialIcons/>
       </Container>
