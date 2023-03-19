223a1e2e14f5b73b3e51d201515caf3c7e740649
DharmaTeja Vytla <teja6d@gmail.com>
2023-03-19 12:23:41 +0000

    Added pwa

---
+++ public/service-worker.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = 'my-pwa-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/script.js',
+  '/images/icon.png'
+];
+
+this.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+this.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+this.addEventListener('activate', event => {
+  const cacheWhitelist = ['my-pwa-cache-v1'];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+         return null;
+        })
+      );
+    })
+  );
+});
